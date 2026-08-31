946385b9ea0393e268c03a7b6a3a5fa271afa050
jlbuild <juliabuildbot@gmail.com>
2026-08-31 23:08:25 +0000

    pandoc_crossref_jll build 3.25.0+0

--- src/pandoc_crossref_jll.jl
+++ src/pandoc_crossref_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("pandoc_crossref")
-JLLWrappers.@generate_main_file("pandoc_crossref", UUID("f96e3c25-ae70-5588-8d8e-4a7ab3ba4c45"))
+JLLWrappers.@generate_main_file("pandoc_crossref", Base.UUID("f96e3c25-ae70-5588-8d8e-4a7ab3ba4c45"))
 end  # module pandoc_crossref_jll
